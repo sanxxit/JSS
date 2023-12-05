2b57fcb563d3acfb59458c8b2d50a2594da5fdc2
Sanchit agrawal <125626091+sanxxit@users.noreply.github.com>
2023-12-05 21:32:25 +0000

    some variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId =1244
+let accountname = "sancit"
+let accountpassword ="2112122"
+accountCity = "delhi"
+accountname = "sancit"
+accountpassword ="2112122"
+let accountaddress;
+console.log(accountId)
+console.table([accountId,accountname,accountpassword,accountCity,accountaddress])
