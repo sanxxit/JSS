70b61e8c2bc381002aa9bc07454e4a7caa6f1711
Sanchit agrawal <125626091+sanxxit@users.noreply.github.com>
2023-12-05 21:54:38 +0000

    standards and datatypes

---
+++ 01_basics/datatypes_ecma_standards.js
@@ -0,0 +1,30 @@
+"use strict"; //use all js code as newer version
+// alert(3+3) // we are usin nodejs not brouwser
+console.log(3
+    +
+    3)//do  not prefer this
+    // code readablility should be high
+
+
+console.log("sanchit")
+let name = "sanchit "
+let age=21
+let isloggedin=false
+
+/*
+datatypes
+number=>2 to pow 53
+bigint
+string
+boolean
+null---datatypes nad also a stand alone value
+undefined --abhi value assign nhi hui hai...there is big  diffrence between null and undefined 
+symbol--uniqueness
+objects
+
+*/
+console.log(typeof"sanchit")
+console.log(typeof"232")
+console.log(typeof 355)
+console.log(typeof null)//object
+console.log(typeof  undefined)//undefined
