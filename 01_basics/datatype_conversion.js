70909bb44f7c4a63cf53576ae5765a72a8ff18e9
Sanchit agrawal <125626091+sanxxit@users.noreply.github.com>
2023-12-06 07:22:56 +0000

    datatype

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,25 @@
+//
+//let age =
+//co//nsole.log(typeof age )
+//let //valueNumber =Number(age)
+//console.log(typeof valueNumber )
+//console.log(valueNumber)
+// "33">=33
+//"33abc"=>NaN
+//true =>1;
+let isloggedin =88
+
+let booleanIsLoggedin =Boolean(isloggedin)
+console.log(booleanIsLoggedin)
+let nameiss =String(isloggedin)
+console.log(nameiss)
+console.log(typeof nameiss)
+/*
+1--true
+0--false
+""--false
+"ram"--true
+"435643"--true
+
+*/
+
