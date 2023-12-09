21eac68d57cf594d7b38f62704aacd7526e70d85
Sanchit agrawal <125626091+sanxxit@users.noreply.github.com>
2023-12-09 09:03:20 +0000

    precendence operators

---
+++ 01_basics/stringtonumber.js
@@ -0,0 +1,16 @@
+let str1="hello"
+let str2= " sanchit"
+let str3 = str1+str2
+console.log(str3)//hello sanchit
+
+//precedence operator....
+
+console.log(1+2)//3
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+2+3)//123
+console.log(1+2+"4")//34
+console.log(true)//true
+console.log(+true)//1
+
+
