667909e49feaad3c5aa9220f3f8c42d62eadef91
Sanchit agrawal <125626091+sanxxit@users.noreply.github.com>
2023-12-11 03:49:32 +0000

    ok

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+//primitive
+//7types--- string,number ,boolean,null,symbol,BigInt
+const score = 100
+const scorevalue =100.5//no diifreent for decimals
+const isloggedin =false
+const outsidetemp=null//abhi uske ander koi temot nhi hai
+let useremail;//undefined ....or u can write    let useremail =undefined;
+const  id =Symbol('4234')
+const anotherid=Symbol('4234')
+console.log(id==anotherid)//false
+const bigNumber = 57657657567674753657547n//bigint
+//symbol convert same things to differnt unique things 
+
+
+//refrence(non primitive)
+//array, objects,functions
+ 
