914401b4275eb8d8f2036823b0e976467e9de4ab
sanxxit <sanchitagrawal2023@gmail.com>
2024-01-01 16:59:43 +0530

    objects

---
+++ cohort0to100/class1.js
@@ -0,0 +1,43 @@
+/*
+let naam="sanchit";
+let age=11
+let ismarried= false;
+if(ismarried==true){
+    console.log(naam +"is married");
+}
+else{
+    console.log(naam +"is not married");
+
+}
+*/
+//arrays---
+//const prsonarray=["ram","shyam","priya"]
+//const genederarray=["male","male","female"]
+
+
+//aaray of objects--complex objects
+/*
+const allusers=[
+    {
+        firstname:"ram",
+        gender:"male"
+
+
+
+
+    }, {
+        firstname:"shyam",
+        gender:"male"
+
+    },{
+        firstname:"geeta",
+        gender:"female"
+    }
+]
+for(let i=0;i<allusers.length;i++) {
+    if(allusers[i]["gender"]=="male"){
+        console.log(allusers[i]["firstname"])
+    }
+
+}
+*/
