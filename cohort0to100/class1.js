8e453645dfd8cf4b4a6315c891c6f9569359d50c
sanxxit <sanchitagrawal2023@gmail.com>
2024-01-02 18:45:34 +0530

    ii

--- cohort0to100/class1.js
+++ cohort0to100/class1.js
@@ -16,6 +16,7 @@ else{
 
 
 //aaray of objects--complex objects
+//array of objects---
 /*
 const allusers=[
     {
@@ -27,7 +28,12 @@ const allusers=[
 
     }, {
         firstname:"shyam",
-        gender:"male"
+        gender:"male",
+        metadeta:{
+            age:21,
+            address:"fds"
+        }
+    
 
     },{
         firstname:"geeta",
@@ -41,3 +47,51 @@ for(let i=0;i<allusers.length;i++) {
 
 }
 */
+//functions--abstract  out logic in your progranm
+//take raguments
+//return value a s an output
+//wmic cpu get NumberOfCores, NumberOfLogicalProcessors/Format:List
+//act like an independent program sthat do somethinf give an input
+//functions can take other functions a s input(callbacks)
+//...😂😂
+
+
+/*
+function findsum(a,b){
+    //do things with input and returmn an output..
+    return a+b;
+}
+const value = findsum(56,99);//calling a function
+console.log(value)
+*/
+/*let sum =0;
+for(let i =0; i<1000000000000;i++){
+    sum = sum +i;
+
+}
+console.log(sum);
+*/
+//let logicalProcessorCount = navigator.hardwareConcurrency;
+
+
+//fnToCall
+//function sum(num1,num2,fnToCall){
+  //  let result = num1+num2;
+    //return result;
+
+
+
+//}
+function calculateArithmetic(a, b, type) {
+
+
+    }
+function sum(a,b){
+    return a+b;
+}
+function minus(a,b){
+    return a-b;
+}
+    
+const value =calculateArithmetic(1,3,"minus");
+console.log(value);
